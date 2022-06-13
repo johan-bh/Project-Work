54bfed2179d0e984c3d00f7417a663643aba042a
Sebastiannfg <73647581+Sebastiannfg@users.noreply.github.com>
2022-06-14 01:52:05 +0200

    Commit before big change

--- TensorRegression.py
+++ TensorRegression.py
@@ -1,55 +1,168 @@
+
+# ---------- IMPORTS ----------
+
+#Torch imports
+import torch
+import torch as nn
+import torch.optim as optim
+torch.manual_seed(0)
+
+#sklearn imports
+from sklearn.model_selection import train_test_split
+
+#Basic dataframe imports
 import numpy as np
 import pandas as pd
 import pickle
-import tltorch
-import torch
-from torch import nn
-import numpy as np
-import tensorly as tl
 
+#Plotting imports
 import matplotlib.pyplot as plt
-# Use tensor regression to reduce size of 3D matrix of size 64x64x7 to single value 64 + 7 = 71
 
+
+#  ---------- COHERENCE AND RESPONSE VARIABLE IMPORT AND HANDLING ----------
 data = pd.read_pickle("data/tensor_data_open.pkl")
 
-#with open('data/response_var_df.pkl', 'rb') as f:
-#    response_var_df = pickle.load(f)
+with open('data/response_var_df.pkl', 'rb') as f:
+    response_var_df = pickle.load(f)
+response_variables = response_var_df.to_dict('dict')
 
 
+"""
+keys = list()
+for key,value in data.items(): #Convert every coherence map into a tensor with key in keys
+    data[key] = torch.tensor(value)
+    keys.append(key)
+"""
 
 keys = list()
 for key,value in data.items(): #Convert every coherence map into a tensor with key in keys
     data[key] = torch.tensor(value)
     keys.append(key)
 
-# Create a Tensor Regression model that takes in a 64x64x7 tensor and outputs a single vector of size 71
-input_shape = (64,64,7)
-batch_size = 32
-x = data["12069"]
-
-#Actual Implementation
 
 
-def Loss_function(Xn, Yn, rank=1):
-    gamma = np.ones((7, 1))
-    alpha = np.ones((64, 1))
+
+#  ---------- DEFINING OUR ADAM OPTIMIZER  ----------
+
+class Regress_Loss(torch.nn.Module):
+
+    def __init__(self):
+        super(Regress_Loss, self).__init__()
+
+    def forward(self, x, y):
+        y_shape = y.size()[1]
+        x_added_dim = x.unsqueeze(1)
+        x_stacked_along_dimension1 = x_added_dim.repeat(1, NUM_WORDS, 1)
+        diff = torch.sum((y - x_stacked_along_dimension1) ** 2, 2)
+        totloss = torch.sum(torch.sum(torch.sum(diff)))
+        return totloss
+
+
+#  ---------- IMPLEMENTATION OF TENSOR REGRESSION  ----------
+# --- CP function ---
+def CPdict(rank):
     CP = {}
     for i in range(rank):
-        CP["gamma" + str(i)] = gamma
-        CP["alpha" + str(i)] = alpha
+        CP["gamma"+str(i)] = {}
+        CP["alpha"+str(i)] = {}
+        for j in range(7):
+            CP["gamma" + str(i)]["g"+str(i)+str(j)] = 1
+        for k in range(64):
+            CP["alpha" + str(i)]["a" + str(i) + str(k)] = 1
+
+        return CP
+
+CPs = CPdict(rank=3)
+
+# --- Cost function ---
+
+def Cost_Function(Xn, Yn, rank, CP_dict):
+
+    finalsum = 0
 
     for k in range(7):
         for j in range(64):
             for i in range(64):
-                if i<=j:
-                    continue
+                if i>j:
+                    CPd_sum=0
 
-                for d in range(rank):
-                    CP["alpha"+str(d)][i]*CP["alpha"+str(d)][i]*CP["beta"+str(d)][j]
+                    for d in range(rank):
+                        CPd_sum += CP["alpha" + str(d)][i]
 
-                Xn[i][j][k]
+                        CPd_sum+=CP["alpha"+str(d)][i]*CP["alpha"+str(d)][i]*CP_dict["gamma"+str(d)][j]
 
-Loss_function(x, 29, rank=2)
+                    finalsum += Xn[i][j][k]*CPd_sum
+
+    return (Yn - beta_0 + finalsum)**2
+
+f = 2
+
+#print(Cost_Function(Xn = x, Yn = 29, rank=2, CP=CP))
+
+# --- Adam optimization ---
+
+
+"""
+if __name__ == '__main__':
+
+    torch.manual_seed(42)
+    X, X_test, y, y_test = Load_data("data/PCA+Y-OPEN.pkl")
+    Xn = X.to_numpy()
+    yn = y.to_numpy()
+    dataset = DataSet(Xn, yn)
+    # dataset = X.to_numpy(), y.to_numpy()[0]
+    trainloader = torch.utils.data.DataLoader(dataset)
+    mlp = MLP()
+
+    # Define the loss function (mean absolute error between input x and target y) and optimizer
+    loss_function = nn.L1Loss()
+    optimizer = torch.optim.Adam(mlp.parameters(), lr=1e-4)
+    count = 0
+
+    # Run the training loop
+    for epoch in range(0, 5):  # 5 epochs at maximum
+        # Print epoch
+        print(f'Starting epoch {epoch + 1}')
+
+        # Set current loss value
+        current_loss = 0.0
+
+        # Iterate over the DataLoader for training data
+        for i, data in enumerate(trainloader, 0):
+
+            # Get and prepare inputs
+            inputs, targets = data
+            inputs, targets = inputs.float(), targets.float()
+            # targets = targets[0].reshape((targets.shape[0], 1))
+
+            # Zero the gradients
+            optimizer.zero_grad()
+
+            # Perform forward pass
+            outputs = mlp(inputs)
+
+            # Compute loss
+            loss = loss_function(outputs, targets)
+
+            # Perform backward pass
+            loss.backward()
+
+            # Perform optimization
+            optimizer.step()
+
+            # Print statistics
+            current_loss += loss.item()
+            if i % 10 == 0:
+                count += 1
+                print('Loss after mini-batch %5d: %.3f' %
+                      (i + 1, current_loss / 155))
+                current_loss = 0.0
+
+    # Process is complete.
+    print('Training process has finished.')
+
+"""
+#  ---------- CURRENTLY UNUSED CODE  ----------
 
 
 
@@ -132,16 +245,10 @@ print(factors.shape)
 new_x = tl.cp_to_tensor(factors)
 
 showTensor(x[:,:,1])
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    set x to device
+x = x.to(device)
+print(x.shape)
+
 """
-
-#Tensor Regression
-
-
-
-
-
-
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# # set x to device
-# x = x.to(device)
-# print(x.shape)
